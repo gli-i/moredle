4a1cbaeee72457b2c733a3e4dbe0c1212ec73de1
gli-i <133687652+gli-i@users.noreply.github.com>
2023-11-14 10:28:08 -0800

    fix navbar classic link

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -21,7 +21,7 @@ export const NavBar = ({ pageWrapId, outerContainerId }: navBarProps) => {
       <Menu>
         <Link className="text-black text-xl font-bold hover:text-green-600 mb-8 transition-all" to={"/"}>Home</Link>
         <Link className="text-black text-xl font-bold hover:text-green-600 mb-8 transition-all" to={"/howtoplay"}>How To Play</Link>
-        <Link className="text-black text-xl font-bold hover:text-green-600 mb-8 transition-all" to={"/"}>Classic</Link>
+        <Link className="text-black text-xl font-bold hover:text-green-600 mb-8 transition-all" to={"/classic"}>Classic</Link>
         <Link className="text-black text-xl font-bold hover:text-green-600 mb-8 transition-all" to={"/timed"}>Timed Mode</Link>
         <Link className="text-black text-xl font-bold hover:text-green-600 transition-all" to={"/stats"}>Player Stats</Link>
 
