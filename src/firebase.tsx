230a3979c94f843d779569223011b1b1e710f99e
gsl5 <gsl5@sfu.ca>
2024-10-14 20:31:33 -0700

    update readme to include instruction on project setup / installation

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,7 +3,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, setDoc, getDoc, updateDoc, DocumentData, increment } from "firebase/firestore";
-import { firebaseConfig } from "./firebaseConfig";
+//import { firebaseConfig } from "./firebaseConfig";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+}
 
 // Initialize Firebase
 
